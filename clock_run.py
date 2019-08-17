5c294e409789ee2fd79b4d282cc15a1d4f60fa61
VictorBoYang <VictorBoYang@gmail.com>
2019-08-17 16:00:06 -0400

    fix time problem

--- clock_run.py
+++ clock_run.py
@@ -5,42 +5,42 @@ from Countdown_Clock import *
 import time
 #user input clock mode
 
-user_input = input("choose the type of clock(press 0 for normal clock, press 1 for count down clock)")
-user_choice = int(user_input)
+user_choice = input("choose the type of clock(press 0 for normal clock, press 1 for count down clock)")
+user_choice = int(user_choice)
 while user_choice != 0 and user_choice != 1:
     print ("you can press only 0 or 1, please enter your choice again")
-    user_input = input("choose the type of clock(press 0 for normal clock, press 1 for count down clock)")
-    user_choice = int(user_input)
+    user_choice = input("choose the type of clock(press 0 for normal clock, press 1 for count down clock)")
+    user_choice = int(user_choice)
 else:
     print ("good")
 
 #user input clock starting hour
-input_hour = input("please enter the starting hour(0-24)")
-clock_hour = int(input_hour)
-while 0 > clock_hour or clock_hour > 24:
-    print ("please enter only the integer between 0 to 24")
-    input_hour = input("please enter the starting hour(0-24)")
-    clock_hour = int(input_hour)
+clock_hour = input("please enter the starting hour(0-23)")
+clock_hour = int(clock_hour)
+while 0 > clock_hour or clock_hour > 23:
+    print ("please enter only the integer between 0 to 23")
+    clock_hour = input("please enter the starting hour(0-23)")
+    clock_hour = int(clock_hour)
 else:
     print ("good")
 
 #user input clock starting Minute
-input_minute = input("please enter the starting minute(0-60)")
-clock_minute = int(input_minute)
-while 0 > clock_minute or clock_minute > 60:
-    print ("please enter only the integer between 0 to 60")
-    input_minute = input("please enter the starting minute(0-60)")
-    clock_minute = int(input_minute)
+clock_minute = input("please enter the starting minute(0-59)")
+clock_minute = int(clock_minute)
+while 0 > clock_minute or clock_minute > 59:
+    print ("please enter only the integer between 0 to 59")
+    clock_minute = input("please enter the starting minute(0-59)")
+    clock_minute = int(clock_minute)
 else:
     print ("good")
 
 #user input clock starting Second
-input_second = input("please enter the starting second(0-60)")
-clock_second = int(input_second)
-while 0 > clock_second or clock_second > 60:
-    print ("please enter only the integer between 0 to 60")
-    input_second = input("please enter the starting second(0-60)")
-    clock_second = int(input_second)
+clock_second = input("please enter the starting second(0-59)")
+clock_second = int(clock_second)
+while 0 > clock_second or clock_second > 59:
+    print ("please enter only the integer between 0 to 59")
+    clock_second = input("please enter the starting second(0-59)")
+    clock_second = int(clock_second)
 else:
     print("good")
 

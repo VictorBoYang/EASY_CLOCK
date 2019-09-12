9a1505856e9b3a75d8dc0632796bef6341d9cdea
VictorBoYang <VictorBoYang@gmail.com>
2019-09-12 12:03:16 -0400

    cleaning, improve all func

--- clock_run.py
+++ clock_run.py
@@ -3,63 +3,26 @@ from class_clock import *
 from Normal_Clock import *
 from Countdown_Clock import *
 import time
-#user input clock mode
 
+#user input clock mode
 user_choice = input("choose the type of clock(press 0 for normal clock, press 1 for count down clock)")
 user_choice = int(user_choice)
-while user_choice != 0 and user_choice != 1:
-    print ("you can press only 0 or 1, please enter your choice again")
-    user_choice = input("choose the type of clock(press 0 for normal clock, press 1 for count down clock)")
-    user_choice = int(user_choice)
-else:
-    print ("good")
+check_user_choice(user_choice)
 
 #user input clock starting hour
 clock_hour = input("please enter the starting hour(0-23)")
 clock_hour = int(clock_hour)
-while 0 > clock_hour or clock_hour > 23:
-    print ("please enter only the integer between 0 to 23")
-    clock_hour = input("please enter the starting hour(0-23)")
-    clock_hour = int(clock_hour)
-else:
-    print ("good")
+check_in_hour(clock_hour)
 
 #user input clock starting Minute
 clock_minute = input("please enter the starting minute(0-59)")
 clock_minute = int(clock_minute)
-while 0 > clock_minute or clock_minute > 59:
-    print ("please enter only the integer between 0 to 59")
-    clock_minute = input("please enter the starting minute(0-59)")
-    clock_minute = int(clock_minute)
-else:
-    print ("good")
+check_in_minute(clock_minute)
 
 #user input clock starting Second
 clock_second = input("please enter the starting second(0-59)")
 clock_second = int(clock_second)
-while 0 > clock_second or clock_second > 59:
-    print ("please enter only the integer between 0 to 59")
-    clock_second = input("please enter the starting second(0-59)")
-    clock_second = int(clock_second)
-else:
-    print("good")
+check_in_second(clock_second)
 
 #clock run
-if user_choice == 0:
-    Clock = Normal_Clock(clock_hour,clock_minute,clock_second)
-    while True:
-        Clock.show()
-        Clock.Tick()
-        time.sleep(1)
-elif user_choice == 1:
-    Clock = Countdown_Clock(clock_hour,clock_minute,clock_second)
-    while True:
-        Clock.show()
-        Clock.Tick()
-        time.sleep(1)
-
-
-# decide_clockType(user_choice)
-# decide_clock_hour(0,24)
-# decide_clock_minute(0,60)
-# decide_clock_second(0,60)
+clock_run(user_choice,clock_hour,clock_minute,clock_second)
--- clock_support.py
+++ clock_support.py
@@ -1,42 +1,55 @@
-def decide_clockType(x):
+from class_clock import *
+from Normal_Clock import *
+from Countdown_Clock import *
+import time
+
+def check_user_choice(x):
+    x = int(x)
     while x != 0 and x != 1:
         print ("you can press only 0 or 1, please enter your choice again")
-        user_input = input("choose the type of clock(press 0 for normal clock, press 1 for count down clock)")
-        x = int(user_input)
+        x = input("choose the type of clock(press 0 for normal clock, press 1 for count down clock)")
+        x = int(x)
     else:
         print ("good")
-        return
 
 
-def decide_clock_hour(x,y):
-    while True:
-        input_hour = input("please enter the starting hour(0-24)")
-        clock_hour = int(input_hour)
-        if x <= clock_hour <= y:
-            print ("good")
-            return clock_hour
-            break
-        else:
-            print ("please enter only the integer between 0 to 24")
+def check_in_hour(hour_in):
+    while 0 > hour_in or hour_in > 23:
+        print ("please enter only the integer between 0 to 23")
+        hour_in = input("please enter the starting hour(0-23)")
+        hour_in = int(hour_in)
+    else:
+        print ("good")
 
 
-def decide_clock_minute(x,y):
-    while True:
-        input_minute = input("please enter the starting minute(0-60)")
-        clock_minute = int(input_minute)
-        if x <= clock_minute <= y:
-            print ("good")
-            break
-        else:
-            print ("please enter only the integer between 0 to 60")
+def check_in_minute(min_in):
+    while 0 > min_in or min_in > 59:
+        print ("please enter only the integer between 0 to 59")
+        min_in = input("please enter the starting minute(0-59)")
+        min_in = int(min_in)
+    else:
+        print ("good")
 
 
-def decide_clock_second(x,y):
-    while True:
-        input_second = input("please enter the starting second(0-60)")
-        clock_second = int(input_second)
-        if x <= clock_second <= y:
-            print ("good")
-            break
-        else:
-            print ("please enter only the integer between 0 to 60")
+def check_in_second(sec_in):
+    while 0 > sec_in or sec_in > 59:
+        print ("please enter only the integer between 0 to 59")
+        sec_in = input("please enter the starting second(0-59)")
+        sec_in = int(sec_in)
+    else:
+        print("good")
+
+
+def clock_run(user_choice,clock_hour,clock_minute,clock_second):
+    if user_choice == 0:
+        Clock = Normal_Clock(clock_hour,clock_minute,clock_second)
+        while True:
+            Clock.show()
+            Clock.Tick()
+            time.sleep(1)
+    elif user_choice == 1:
+        Clock = Countdown_Clock(clock_hour,clock_minute,clock_second)
+        while True:
+            Clock.show()
+            Clock.Tick()
+            time.sleep(1)
